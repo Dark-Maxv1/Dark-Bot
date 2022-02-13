430ecb67e8b06ae2baa70e7c3dba189e9dbdda5b
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-13 14:17:32 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -25,13 +25,13 @@ if (Config.WORKTYPE == 'private') {
 const btn = {
    // imageMessage: 'https://telegra.ph/file/cd6032c65c27e0510ddad.jpg',
     contentText: "♥️💫 HOW ARE YOU 🙈♥️",
-    footerText: 'BY ⎝🛡️ ALPHA 🛡️⎠ ▷',
+    footerText: 'BY ☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️ ▷',
     buttons: buttons,
     headerType: 1
 }
 
             
-            var image = await axios.get ('https://telegra.ph/file/cd6032c65c27e0510ddad.jpg', {responseType: 'arraybuffer'})
+            var image = await axios.get ('https://telegra.ph/file/91189e6a925683868095d.jpg', {responseType: 'arraybuffer'})
        
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: " 🔮 Hay All I'm Alive now 🔮\n\n🧿 My name : ⎝🛡️ ALPHA 🛡️⎠\n\n\n ◐. Developer: ACHI FERNANDO \n ◐. Github - https://bit.ly/3L8TVwS \n ◐. Youtube - https://bit.ly/3IeLP3Q \n ◐. Owner - https://bit.ly/3A5DTyQ \n ◐. Our Web - https://bit.ly/3Kig1Na \n\n ◐. All Cammands -Type .alpha\n\n\n🖲️Thank You For Using ⎝🛡️ ALPHA 🛡️⎠"})
             
@@ -47,7 +47,7 @@ const btn = {
 const btn = {
    // imageMessage: 'https://telegra.ph/file/cd6032c65c27e0510ddad.jpg',
     contentText: "♥️💫 HOW ARE YOU 🙈♥️",
-    footerText: 'BY ⎝🛡️ ALPHA 🛡️⎠ ▷',
+    footerText: 'BY ☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️ ▷',
     buttons: buttons,
     headerType: 1
 }
@@ -57,7 +57,7 @@ const btn = {
             
             var image = await axios.get ( Config.AL , {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*⎝🛡️ ALPHA 🛡️⎠*' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️*' })
         await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
 
      }
@@ -101,13 +101,13 @@ else if (Config.WORKTYPE == 'public') {
 const btn = {
    // imageMessage: 'https://telegra.ph/file/cd35928cef2be17d339c1.jpg',
     contentText: "♥️💫 HOW ARE YOU 🙈♥️",
-    footerText: 'BY ⎝🛡️ ALPHA 🛡️⎠ ▷',
+    footerText: 'BY ☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️ ▷',
     buttons: buttons,
     headerType: 1
 }
 
             
-            var image = await axios.get ('https://telegra.ph/file/cd6032c65c27e0510ddad.jpg', {responseType: 'arraybuffer'})
+            var image = await axios.get ('https://telegra.ph/file/91189e6a925683868095d.jpg', {responseType: 'arraybuffer'})
        
 
        await message.client.updatePresence(message.jid,Presence.composing)
@@ -115,7 +115,7 @@ const btn = {
         await new Promise(r => setTimeout(r, 1000));
 
  
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "🔮 Hay All I'm Alive now 🔮\n\n🧿 My name : ⎝🛡️ ALPHA 🛡️⎠\n\n\n ◐. Developer: ACHI FERNANDO \n ◐. Github - https://bit.ly/3L8TVwS \n ◐. Youtube - https://bit.ly/3IeLP3Q \n ◐. Owner - https://bit.ly/3A5DTyQ \n ◐. Our Web - https://bit.ly/3Kig1Na \n\n ◐. All Cammands -Type .alpha\n\n\n🖲️Thank You For Using ⎝🛡️ ALPHA 🛡️⎠"})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "🔮 Hay All I'm Alive now 🔮\n\n🧿 My name : ☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️ \n\n\n ◐. Developer: ACHI FERNANDO \n ◐. Github - https://bit.ly/3L8TVwS \n ◐. Youtube - https://bit.ly/3IeLP3Q \n ◐. Owner - https://bit.ly/3A5DTyQ \n ◐. Our Web - https://bit.ly/3Kig1Na \n\n ◐. All Cammands -Type .alpha\n\n\n🖲️Thank You For Using ⎝🛡️ ALPHA 🛡️⎠"})
             
         await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
 
@@ -129,7 +129,7 @@ const btn = {
 const btn = {
    // imageMessage: 'https://telegra.ph/file/cd6032c65c27e0510ddad.jpg',
     contentText: "♥️💫 HOW ARE YOU 🙈♥️",
-    footerText: 'BY ⎝🛡️ ALPHA 🛡️⎠ ▷',
+    footerText: 'BY ☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️ ▷',
     buttons: buttons,
     headerType: 1
 }
@@ -145,7 +145,7 @@ const btn = {
         await new Promise(r => setTimeout(r, 1000));
 
  
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*⎝🛡️ ALPHA 🛡️⎠*' })
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*☠️𝘿𝘼𝙍𝙆 𝘽𝙊𝙏☠️*' })
         await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
 
      }
